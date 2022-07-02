e6b6821763f26ec28a8dcbfd79ceaac0e9f82275
StefanGreve <greve.stefan@outlook.jp>
2022-07-02 22:49:00 +0200

    Remove RunCommand, and use CliWrap nuget instead

--- src/Private/Utils.cs
+++ src/Private/Utils.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Linq;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -6,44 +8,6 @@ namespace RepoManager
 {
     internal class Utils
     {
-        public static List<string> RunCommand(string workingDirectory, string program, params string[] arguments)
-        {
-            var tsc = new TaskCompletionSource<int>();
-            List<string> standardOutput = new List<string>();
 
-            var startInfo = new ProcessStartInfo
-            {
-                WorkingDirectory = workingDirectory,
-                FileName = program,
-                Arguments = string.Join(" ", arguments),
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            };
-
-            using (var process = new Process { StartInfo = startInfo, EnableRaisingEvents = true })
-            {
-                process.Start();
-
-                process.Exited += (sender, args) =>
-                {
-                    tsc.SetResult(process.ExitCode);
-                    process.Dispose();
-                };
-
-                process.OutputDataReceived += (sender, line) =>
-                {
-                    if (line.Data != null)
-                    {
-                        standardOutput.Add(line.Data);
-                    }
-                };
-
-                process.BeginOutputReadLine();
-                process.WaitForExit();
-            }
-
-            return standardOutput;
-        }
     }
 }
