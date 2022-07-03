f026fd22be732b20417f508abc58fce111877f71
StefanGreve <greve.stefan@outlook.jp>
2022-07-04 00:28:44 +0200

    Re-Implement (static) Utils class
    * RemoveLineBreaks
    * ForEach (Linq extension method)
    * DeleteDirectoryRecursively (accounts for hidden files)
    * GetAllRepositoryNames (for GitHub)

--- src/Private/Utils.cs
+++ src/Private/Utils.cs
@@ -1,13 +1,53 @@
+using System;
 using System.IO;
 using System.Linq;
-using System.Diagnostics;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Octokit;
+
 namespace RepoManager
 {
-    internal class Utils
+    internal static class Utils
     {
+        public static string RemoveLineBreaks(this string @string)
+        {
+            return @string.Replace("\r\n", string.Empty).Replace("\r", string.Empty).Replace("\n", string.Empty);
+        }
 
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+
+            return enumeration;
+        }
+
+        public static void DeleteDirectoryRecursively(string path)
+        {
+            foreach (string file in Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories))
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string directory in Directory.EnumerateDirectories(path))
+            {
+                var directoryInfo = new DirectoryInfo(directory);
+                directoryInfo.Attributes |= FileAttributes.Normal;
+                Directory.Delete(directory, recursive: true);
+            }
+
+            Directory.Delete(path);
+        }
+
+        public static async Task<IEnumerable<string>> GetAllRepositoryNames(string username)
+        {
+            var github = new GitHubClient(new Octokit.ProductHeaderValue(ConfigurationManager.ModuleName));
+            var repositories = await github.Repository.GetAllForUser(username);
+            return repositories.Select(repo => repo.Name);
+        }
     }
 }
