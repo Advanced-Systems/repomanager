da5921ef4fefa60c71cb0d22b1b8712333d23bb1
StefanGreve <greve.stefan@outlook.jp>
2023-04-07 20:57:28 +0200

    Update examples (renamed aliases and options)

--- examples/examples.ps1
+++ examples/examples.ps1
@@ -5,22 +5,22 @@
 #>
 
 # track all remote branches from the exercises repository
-clone -User StefanGreve -Name exercises
+clone -User StefanGreve -Repository exercises
 track exercises -All
 
 # open the dtui repository in a new VS Code window
-code -n $(grepo dtui).Path
+code -n $(repo dtui).Path
 
 # clone all repositories from StefanGreve to desktop and track all branches everywhere
 $desktop = [Environment]::GetFolderPath("Desktop")
-clone -Path $desktop -User stefangreve -Protocoll SSH -TrackAllBranches -All
+clone -Container $desktop -User stefangreve -Protocoll SSH -TrackAllBranches -All
 
 # query all repositories in the default container, and dry-run the removal command afterwards
-$repos = grepo -All | select -ExpandProperty Name
+$repos = repo -All | select -ExpandProperty Name
 $repos | rmrepo -WhatIf
 
 # retrieve the author's email address from the last commit
-$lastCommit = grepo dtui | select -ExpandProperty LastCommit
+$lastCommit = repo dtui | select -ExpandProperty LastCommit
 echo $lastCommit.Author.Email
 
 # inspect the diff of the last commit in repomanager
