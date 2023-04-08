edb2b0c825a679582c2d5ebf57f6416a5ccfc0d4
StefanGreve <greve.stefan@outlook.jp>
2023-04-08 11:34:47 +0200

    Span process invocation into multiple lines and maximize terminal window on startup

--- scripts/import.ps1
+++ scripts/import.ps1
@@ -1,1 +1,10 @@
-Start-Process pwsh -ArgumentList "-NoProfile -NoLogo -NoExit", "-Command &{ cd .\src && dotnet publish && Import-Module .\bin\Debug\netstandard2.1\publish\RepoManager.dll -Force }"
+$Arguments = @{
+    FilePath = "pwsh"
+    WindowStyle = "Maximized"
+    ArgumentList = @(
+        "-NoProfile -NoLogo -NoExit"
+        "-Command &{ cd .\src && dotnet publish && Import-Module .\bin\Debug\netstandard2.1\publish\RepoManager.dll -Force }"
+    )
+}
+
+Start-Process @Arguments
